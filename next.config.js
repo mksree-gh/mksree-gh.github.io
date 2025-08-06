c39ba280982b26efd55c49c6752dbdc61469fb26
Keerthi Sree Marrapu <mksree@MacBook-Air.local>
2025-08-07 01:29:37 +0530

    fixing deployment errors 2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
+
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    unoptimized: true,
   },
-  images: { unoptimized: true },
+  assetPrefix: '/mksree-gh.github.io',
+  basePath: '/mksree-gh.github.io',
 };
 
 module.exports = nextConfig;
